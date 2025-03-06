2bf8263ebcd92728f1bc1b3f4bcfc9cff8418376
Anthony Fu <github@antfu.me>
2025-03-06 18:17:04 +0800

    feat(eslint): expose recommended config

--- packages/eslint-plugin-pnpm-catalogs/src/index.ts
+++ packages/eslint-plugin-pnpm-catalogs/src/index.ts
@@ -1,8 +1,32 @@
 import type { ESLint } from 'eslint'
+import jsoncParser from 'jsonc-eslint-parser'
 import { rules } from './rules'
 
 export const plugin: ESLint.Plugin = {
   rules,
 }
 
+export const configs = {
+  recommended: [
+    {
+      name: 'pnpm-catalogs:package.json',
+      files: [
+        'package.json',
+        '**/package.json',
+      ],
+      languageOptions: {
+        parser: jsoncParser,
+      },
+      plugins: {
+        'pnpm-catalogs': plugin,
+      },
+      rules: {
+        'pnpm-catalogs/enforce-catalog': 'error',
+        'pnpm-catalogs/valid-catalog': 'error',
+      },
+    },
+  ],
+} satisfies ESLint.Plugin['configs']
+
+plugin.configs = configs
 export default plugin
